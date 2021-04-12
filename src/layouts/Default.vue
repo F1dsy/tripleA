8b2d6c25b795ea73c1c27be6e63588ebcb890281
Alexander Bang <>
2021-04-12 22:46:04 +0200

    round menu

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -10,6 +10,7 @@
           <li><a href="/blog">Blog</a></li>
           <li><a href="">Om os</a></li>
         </ul>
+        <div class="splash"></div>
       </div>
       <svg
         viewBox="0 0 80 57"
@@ -67,7 +68,6 @@ nav {
   height: 80px;
   z-index: 1;
   padding: 0 20px;
-  transition: 300ms;
   background-color: $color3;
   box-shadow: 0 0 10px #0005;
   &.scrolled {
@@ -83,17 +83,25 @@ nav {
     top: 0;
     right: 0;
     position: fixed;
-    transition: width 500ms;
-    width: 0;
     z-index: 1;
+    // width: 0;
+    // height: 0;
+    width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.9);
-    overflow: hidden;
-    border-radius: 50%;
+    pointer-events: none;
     &.open {
-      width: 100%;
+      pointer-events: unset;
+      .splash::after {
+        transform: scale(1);
+      }
+      ul,
+      .close {
+        opacity: 1;
+      }
     }
     .close {
+      // display: none;
+      opacity: 0;
       position: absolute;
       top: 25px;
       right: 30px;
@@ -106,12 +114,39 @@ nav {
       height: 100%;
       justify-content: space-evenly;
       align-items: center;
+      opacity: 0;
+      transition: opacity 1s;
       li {
         font-size: 1.5em;
         a {
           text-decoration: none;
           color: white;
         }
+      }
+    }
+
+    .splash {
+      position: absolute;
+      top: 40px;
+      right: 40px;
+      width: 0px;
+      height: 0px;
+
+      &::after {
+        content: "";
+        display: block;
+        position: absolute;
+        border-radius: 50%;
+        background-color: rgba(0, 0, 0, 0.9);
+        width: 284vmax;
+        height: 284vmax;
+        top: -142vmax;
+        left: -142vmax;
+        transform: scale(0);
+        z-index: -1;
+        transform-origin: 50% 50%;
+        // transition: transform 0.5s cubic-bezier(0.755, 0.05, 0.855, 0.06);
+        transition: transform 1s;
       }
     }
   }
